f224efeea53e7f2497fe820c0820b781cfced535
Michael Webster <miketwebster@gmail.com>
2015-03-11 16:07:12 -0400

    more

--- files/usr/lib/linuxmint/mintBluetooth/mint-bluetooth.py
+++ files/usr/lib/linuxmint/mintBluetooth/mint-bluetooth.py
@@ -6,11 +6,16 @@ import gettext
 from gi.repository import Gtk, GnomeBluetooth
 import pwd
 import socket
+import subprocess
 
 BLUETOOTH_DISABLED_PAGE      = "disabled-page"
 BLUETOOTH_HW_DISABLED_PAGE   = "hw-disabled-page"
 BLUETOOTH_NO_DEVICES_PAGE    = "no-devices-page"
 BLUETOOTH_WORKING_PAGE       = "working-page"
+
+RFKILL_CHK = ["/usr/sbin/rfkill", "list", "bluetooth"]
+RFKILL_BLOCK = ["/usr/sbin/rfkill", "block", "bluetooth"]
+RFKILL_UNBLOCK = ["/usr/sbin/rfkill", "unblock", "bluetooth"]
 
 # i18n
 gettext.install("cinnamon", "/usr/share/locale")
@@ -23,6 +28,7 @@ class BluetoothConfig:
         self.window.set_title(_("Bluetooth"))
         self.window.set_icon_name("bluetooth")
         self.window.connect("destroy", Gtk.main_quit)
+        self.window.set_default_size(640, 480)
 
         self.main_box = Gtk.VBox()
         self.stack = Gtk.Stack()
@@ -40,8 +46,24 @@ class BluetoothConfig:
         self.stack.show();
         self.main_box.show();
 
-        self.main_box.pack_start(self.stack, True, True, 0)
-        self.window.add(self.main_box)
+        switchbox = Gtk.HBox()
+        switchbox.pack_end(self.rf_switch, False, False, 6)
+        switchbox.show_all()
+
+        self.main_box.pack_start(switchbox, False, False, 6)
+        self.main_box.pack_start(self.stack, True, True, 6)
+
+        frame = Gtk.Frame()
+        frame.add(self.main_box)
+        frame.set_border_width(6)
+        frame.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+        frame.show()
+
+        self.window.add(frame)
+
+        self.check_airplane_mode()
+
+        self.rf_switch.connect("state-set", self.on_switch_changed)
 
         self.window.show()
 
@@ -49,6 +71,32 @@ class BluetoothConfig:
         label = Gtk.Label(message)
         self.stack.add_named(label, name)
         label.show()
+
+    def check_airplane_mode(self):
+        res = subprocess.check_output(RFKILL_CHK)
+
+        if not res:
+            self.stack.set_visible_child_name (BLUETOOTH_NO_DEVICES_PAGE);
+            self.rf_switch.set_sensitive(False)
+
+    def on_switch_changed(self, state, data):
+        finish_state = False
+
+        try:
+            if state == True:
+                res = subprocess.check_output(RFKILL_UNBLOCK)
+            else:
+                res = subprocess.check_output(RFKILL_BLOCK)
+
+            if not res:
+                res = subprocess.check_output(RFKILL_CHK)
+        except CalledProcessError:
+            finish_state = False;
+
+        # figure out if it worked...
+
+        self.rf_switch.set_state(finish_state)
+        return True
 
 if __name__ == "__main__":
     BluetoothConfig()
